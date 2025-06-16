88ec3d095cc2fb8200d6eceb2f5f45917612debb
ACELEST <2401600193@kluniversity.in>
2025-06-16 19:21:14 +0530

    Refactor code structure for improved readability and maintainability

--- my-medusa-store-storefront/src/modules/home/components/hero/index.tsx
+++ my-medusa-store-storefront/src/modules/home/components/hero/index.tsx
@@ -1,33 +1,29 @@
-import { Github } from "@medusajs/icons"
-import { Button, Heading } from "@medusajs/ui"
-
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            Ecommerce Starter Template
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            Powered by Medusa and Next.js
-          </Heading>
-        </span>
-        <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
-          target="_blank"
-        >
-          <Button variant="secondary">
-            View on GitHub
-            <Github />
-          </Button>
-        </a>
+    <div className="h-[65vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+      {/* Image desktop */}
+      <img
+        src="/hero/herolargetall.jpg"
+        alt="Apple rentrée desktop"
+        className="absolute inset-0 z-0 hidden object-cover w-full h-full sm:block"
+      />
+      {/* Image mobile */}
+      <img
+        src="/hero/hero1.jpg"
+        alt="Apple rentrée mobile"
+        className="absolute inset-0 z-0 block object-cover w-full h-full sm:hidden"
+      />
+
+      {/* Contenu positionné manuellement */}
+      <div className="absolute inset-0 z-10 flex flex-col items-center gap-6 text-center">
+        {/* Positionnement responsive du bouton : plus bas en desktop */}
+        <div className="mt-[50vh] sm:mt-[50vh]">
+          <a href="/collections/apple">
+            <button className="px-8 py-3 text-lg font-semibold text-black transition-all duration-200 border rounded-full shadow-lg bg-white/90 hover:bg-white border-white/80 backdrop-blur-md">
+              Voir la collection
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   )
